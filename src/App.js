cf94c9b337458ec28cbee2058316909484b73dea
Chanon Yaklai <taforyou@Chanons-MacBook-Pro.local>
2019-04-12 23:13:48 +0700

    Step-3 [Frontend] Design component and state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,97 @@
 import React, { Component } from "react";
 import "./App.css";
-import { Button } from "antd";
+import { Input, Icon, Button, Card, List, Checkbox } from "antd";
+
+// Step 3 - Design component and state
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputText: "",
+      todos: [
+        {
+          id: "1",
+          title: "Title1",
+          completed: true,
+          selected: true
+        },
+        {
+          id: "2",
+          title: "Title2",
+          completed: false,
+          selected: false
+        }
+      ],
+      isLoading: false
+    };
+  }
+
   render() {
     return (
       <div className="App">
-        <Button type="primary">ทดสอบปุ่ม</Button>
+        <Card>
+          <h1>To-do-list</h1>
+
+          <div style={{ marginBottom: "10px" }}>
+            <Input.Search
+              placeholder="input search text"
+              enterButton="Add"
+              size="large"
+            />
+          </div>
+
+          <List
+            bordered
+            dataSource={this.state.todos}
+            style={{ height: 300, overflow: "auto" }}
+            loading={this.state.isLoading}
+            header={
+              <div style={{ position: "relative", height: 20 }}>
+                <Checkbox
+                  style={{ marginRight: 10, position: "absolute", left: 0 }}
+                >
+                  Select All
+                </Checkbox>
+                <Button.Group style={{ position: "absolute", right: 0 }}>
+                  <Button type="dashed" size="small" style={{ marginRight: 5 }}>
+                    All
+                  </Button>
+                  <Button type="dashed" size="small" style={{ marginRight: 5 }}>
+                    Active
+                  </Button>
+                  <Button
+                    type="danger"
+                    size="small"
+                    style={{
+                      display: this.state.isLoading ? "none" : " ",
+                      marginLeft: 10
+                    }}
+                  >
+                    Clear completed
+                  </Button>
+                </Button.Group>
+              </div>
+            }
+            renderItem={todo => (
+              <List.Item
+                actions={[
+                  <Icon
+                    type="close-circle"
+                    style={{ fontSize: 16, color: "rgb(255, 145, 0)" }}
+                  />
+                ]}
+                style={{
+                  textDecorationLine: todo.completed ? "line-through" : "none"
+                }}
+              >
+                <Checkbox checked={todo.selected} style={{ marginRight: 10 }} />
+                <h4>{todo.title}</h4>
+              </List.Item>
+            )}
+          />
+        </Card>
       </div>
     );
   }
